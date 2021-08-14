50c03b6a4378e6dd9099f0a3c2190331bd9b3e2c
Andreagrll <A01742098@itesm.mx>
2021-08-14 16:30:38 +0000

    Terminado

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,7 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    #Modifica el programa para que realice lo siguiente: 
+    #En una universidad cada estudiante cursa 4 materias en el semestre. 
+    #Desarrolla un programa que reciba la calificación de cada materia (tipo float), 
+    #calcula el promedio de las 4 materias y lo despliega.
 
+    cali1= float(input('Calificación de la materia: '))
+    cali2= float(input('Calificación de la materia: '))
+    cali3= float(input('Calificación de la materia: '))
+    cali4= float(input('Calificación de la materia: '))
 
+    promedio= (cali1+cali2+cali3+cali4)/4
+
+    print('El promedio es: '+ str(promedio))
+    
 if __name__ == '__main__':
     main()
