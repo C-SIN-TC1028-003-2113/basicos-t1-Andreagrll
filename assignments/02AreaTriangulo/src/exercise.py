e18932579429ba4b32fefa5802e1fcc14447e7c5
Andreagrll <A01742098@itesm.mx>
2021-08-14 16:15:38 +0000

    Terminado :)

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,6 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    #Modifica el programa para que lea los datos tipo float: 
+    #base (b) y altura (h), y muestre el área del triángulo.
+    
+    b= float(input('Dame la base: '))
+    h= float(input('Dame la altura: '))
+
+    area= (b*h)/2
+
+    print('El área es: ' +str(area))
 
 if __name__ == '__main__':
     main()
